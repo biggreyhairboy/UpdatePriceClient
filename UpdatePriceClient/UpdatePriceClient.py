03e8941e53ad17e2fd37346630b932c8d27df1de
biggreyhairboy <biggreyhairboy@gmail.com>
2015-12-29 19:14:59 +0800

    可以推送数据到server的数据库中

---
+++ UpdatePriceClient/UpdatePriceClient.py
@@ -0,0 +1,25 @@
+﻿from socket import *
+from WindPy import w
+from datetime import *
+
+w.start()
+host = '128.199.165.107'
+port = 9001
+bufsize = 1024
+addr = (host,port)
+client = socket(AF_INET,SOCK_STREAM)
+client.connect(addr)
+while True:
+    #data = input("price: ")
+    data = w.wsq("AG1606.SHF", "rt_last")
+    #print data
+    realtimeprice = data.Data[0][0]
+    print realtimeprice
+    print('\r\n')
+    if not data or data=='exit':
+        break
+    client.send('%s\r\n' % realtimeprice)
+    data = client.recv(bufsize)
+    if not data:
+        break
+client.close()
